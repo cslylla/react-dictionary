43c2357e6e6a24be15418691454f00b3a5a4061a
cslylla <81328857+cslylla@users.noreply.github.com>
2021-06-01 15:30:34 +0200

    Created initial files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import start from "./images/start.svg"
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> 
+      <img src={start} alt="Reading woman drawing"/>
+      </header>
+    </div>
+  );
+}
