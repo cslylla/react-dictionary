50491a9c37bebede88fb2c08a8614a2a6ca80d28
cslylla <81328857+cslylla@users.noreply.github.com>
2021-06-01 16:11:43 +0200

    Creating Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import start from "./images/start.svg"
+import start from "./images/start.svg";
+import Dictionary from "./Dictionary";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+    <div className="container">
 
 <header className="App-header"> 
       <img src={start} alt="Reading woman drawing"/>
 </header>
 
 <main>
-
+    <Dictionary />
 </main>
 
 <footer>
@@ -18,6 +20,7 @@ export default function App() {
         This is an <a href="https://github.com/cslylla/react-dictionary" target="_blank" rel="noreferrer" title="Open GitHub repository">open-sourced</a> project coded by <a href="https://www.linkedin.com/in/lilla-csanaky-a850a617/" target="_blank" rel="noreferrer" title="Open Lilla`s LinkedIn profile" >cslylla</a> 
         </p>
 </footer>
+    </div>
     </div>
   );
 }
