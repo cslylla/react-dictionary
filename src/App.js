5f58e35d34846d1237526ee1e9043f6925ae383e
cslylla <81328857+cslylla@users.noreply.github.com>
2021-06-01 16:06:07 +0200

    Adding bootstrap, fonts and creating the footer

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"> 
+
+<header className="App-header"> 
       <img src={start} alt="Reading woman drawing"/>
-      </header>
+</header>
+
+<main>
+
+</main>
+
+<footer>
+        <p className="m-0">
+        This is an <a href="https://github.com/cslylla/react-dictionary" target="_blank" rel="noreferrer" title="Open GitHub repository">open-sourced</a> project coded by <a href="https://www.linkedin.com/in/lilla-csanaky-a850a617/" target="_blank" rel="noreferrer" title="Open Lilla`s LinkedIn profile" >cslylla</a> 
+        </p>
+</footer>
     </div>
   );
 }
