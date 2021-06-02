8c85c7bbb91f04798b7408a46174746473557723
cslylla <81328857+cslylla@users.noreply.github.com>
2021-06-02 16:00:31 +0200

    Adding synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 import "./Meaning.css"
 
@@ -8,14 +9,26 @@ export default function Meaning(props){
         <div className="Meaning">
             <h3><strong>{props.meaning.partOfSpeech}</strong></h3>
             {props.meaning.definitions.map(function(definition, index){
-                return(
+                if (definition.example) {
+                    return(
                     <div key={index}>
                     <hr />
                     <p><strong>Definition: </strong>{definition.definition}</p>
-                    <p><strong>Example:</strong> <em>"{definition.example}"</em></p>
+                    <p><strong>Example:</strong> <em>{definition.example}</em></p>
+                    <Synonyms synonyms={definition.synonyms} />
                     </div> 
                     
                 )
+                } else {}
+                    return(
+                    <div key={index}>
+                    <hr />
+                    <p><strong>Definition: </strong>{definition.definition}</p>
+                    <Synonyms synonyms={definition.synonyms} />
+                    </div> 
+                    
+                )
+                
             })}
             
             
