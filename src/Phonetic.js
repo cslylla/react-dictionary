305e47394820bed19185bfd13a70c5c6fd021b49
cslylla <81328857+cslylla@users.noreply.github.com>
2021-06-03 22:29:31 +0200

    Adding button for playing phonetics and formatting it

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,11 +3,18 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props){
+    let url = props.phonetic.audio;
+    let audioObj = new Audio(url);
+
+    function play(event){
+        event.preventDefault();
+        audioObj.play();
+    }
+
     return (
         <div className="Phonetic">
         <hr />   
-        <h3>{props.phonetic.text}</h3>
-        <a href={props.phonetic.audio} target="_blank" rel="noreferrer" title="Listen phonetic">Listen</a> 
+        <h3><i className="fas fa-play-circle play" onClick={play} tabIndex="0"></i> {props.phonetic.text}</h3>
         </div>
        
     )
