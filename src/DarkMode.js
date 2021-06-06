abc9e9e645d82df46cf36b4e8fe324ef042af6fd
cslylla <81328857+cslylla@users.noreply.github.com>
2021-06-06 15:54:23 +0200

    Adding footer and section color to the dark mode

--- src/DarkMode.js
+++ src/DarkMode.js
@@ -5,7 +5,6 @@ import "./DarkMode.css";
 export default function DarkMode(){
     let clickedClass = "clicked";
     const body = document.body;
-    const footer = document.querySelectorAll("footer");
     const lightTheme = "light";
     const darkTheme = "dark";
     let theme;
@@ -16,23 +15,19 @@ export default function DarkMode(){
 
     if (theme === lightTheme || theme === darkTheme){
         body.classList.add(theme);
-        footer.classList.add(theme);
     } else {
         body.classList.add(lightTheme);
-        footer.classList.add(lightTheme);
     }
 
 
     const switchTheme = (e) => {
         if(theme === darkTheme){
             body.classList.replace(darkTheme, lightTheme);
-            footer.classList.replace(darkTheme, lightTheme);
             e.target.classList.remove(clickedClass);
             localStorage.setItem("theme", "light");
             theme=lightTheme;
         } else{
             body.classList.replace(lightTheme, darkTheme);
-            footer.classList.replace(lightTheme, darkTheme);
             e.target.classList.add(clickedClass);
             localStorage.setItem("theme", "dark");
             theme=darkTheme;
